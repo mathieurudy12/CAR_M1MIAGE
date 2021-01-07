cb0212314365d746135e0cff9720a4b02d6fbf05
mathieurudy12 <66941346+mathieurudy12@users.noreply.github.com>
2021-01-07 11:57:33 +0100

    Create Main.java

---
+++ tp1/Main.java
@@ -0,0 +1,25 @@
+
+/**
+ * Programme principal de l'application
+ *
+ */
+public class Main 
+{
+
+    /**
+     * Lancement de l'application
+     *
+     * @param args
+     */
+    public static void main(String[] args) 
+    {
+        try 
+        {
+
+        } 
+        catch(Exception exception) 
+        {
+            System.out.println("Error: " + exception.getMessage());
+        }
+    }
+}
